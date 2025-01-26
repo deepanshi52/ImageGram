39dca9941070790f9e3aecf1cee5124e46d66562
deepanshi sharma <deepanshis013@gmail.com>
2025-01-27 01:39:05 +0530

    Added middleware demo

---
+++ src/Controllers/postControllers.js
@@ -0,0 +1,5 @@
+export async function createPost(req, res){
+// call the service layer
+    return res.json({message: 'Post created successfully'})
+
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
+import { createPost } from './Controllers/postControllers.js';
 
 const PORT = 3002; // port number
 const app = express(); // create express app server instance
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.send('Home');
 })
 
-app.get('/ping', (req, res) => {
-    return res.json({message: 'pong'});
-})
+app.get('/ping/:name', (req, res) => {
+    //console.log(req.query);
+    console.log(req.body);
+    
+    // const name = req.params.name
+    return res.json({message: 'pong' + ' ' + name });
+});
 
 app.get('/hello', (req, res) => {
      return res.json({message: 'Hello World!' });
@@ -20,6 +27,22 @@ app.post('/hello', (req, res) => {
     return res.json({message: 'POST: Hello World!'});
 })
 
+function m1(req, res, next){
+    console.log('m1');
+    next();   
+}
+
+function m2(req, res, next){
+    console.log('m2');
+    next();    
+}
+
+function m3(req, res, next){
+    console.log('m3');
+    next();   
+}
+
+app.post('/posts',[m1, m2 , m3 , createPost ]);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
