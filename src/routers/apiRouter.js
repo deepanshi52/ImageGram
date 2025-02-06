bc1259c3867400437678108364558e82e323e854
deepanshi sharma <deepanshis013@gmail.com>
2025-02-06 23:29:56 +0530

    Added routing layer

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,10 +1,12 @@
 // this api router will be triggered when any request starting with /api comes
-import postRouter from './post.js'
-
+import postRouter from './post.js';
+import userRouter from './user.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.use('/posts',postRouter); // if in the remaining URL, AFTER/api we have the url starting with /posts,then the reqyest is forwarded to postrouter
+
+router.use('/users',userRouter)
 
 export default router;
